585f889b6bc41689a1cd338836b5614099941acb
Har-zheng <32183562+Har-zheng@users.noreply.github.com>
2021-12-02 09:40:45 +0800

    Update httpLog.js
    log

--- app/middleware/httpLog.js
+++ app/middleware/httpLog.js
@@ -22,3 +22,23 @@ module.exports = options => {
     fs.appendFileSync(ctx.app.baseDir + '/httpLog.log', data);
   };
 };
+// 交易组件, 小程序新支付校验支付成功后, 调用同步订单支付结果失败, 一直提示订单不存在 求解答 ?
+
+// 请求参数
+openid: "oU-wC5CnQ9qWxt1u8CgyzPo9NqXk"
+out_order_id: "6696f86bdf-m6blw202109220919007"
+//响应参数
+errcode: 1010011
+errmsg: "订单不存在 rid: 614a84bd-1107c3f9-782dc0b3"
+
+// 并且使用最新的支付校验 success 成功后直接调用同步订单支付结果 就出现上面的 订单不存在  很疑惑啊  大佬们
+wx.requestOrderPayment({
+  timeStamp: '',
+  nonceStr: '',
+  package: '',
+  signType: 'MD5',
+  paySign: '',
+  orderInfo: { ...}, // 需要新增的 订单 信息
+  success(res) { },
+  fail(res) { }
+})
